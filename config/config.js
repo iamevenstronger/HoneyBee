26c30d99b4cb376e3667bd571a4d9cc54a23176d
evenstronger <31339549+iamevenstronger@users.noreply.github.com>
2018-03-12 00:18:32 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,20 +4,20 @@ module.exports = {
     "password": "",
     "database": "honeycomb",
     "host": "localhost",
-    "dialect": "postgres"
+    "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": "root",
     "password": "",
     "database": "honeycomb",
     "host": "localhost",
-    "dialect": "postgres"
+    "dialect": process.env.DB_DIALECT
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": process.env.DB_DIALECT
   }
 };
