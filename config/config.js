c7b99ca36974b536ca755009d5dd90c650cdc57f
evenstronger <31339549+iamevenstronger@users.noreply.github.com>
2018-03-12 00:08:16 +0530

    Sql changes

--- config/config.js
+++ config/config.js
@@ -4,20 +4,20 @@ module.exports = {
     "password": "",
     "database": "honeycomb",
     "host": "localhost",
-    "dialect": "mysql"
+    "dialect": 'mysql'|'sqlite'|'postgres'|'mssql'
   },
   "test": {
     "username": "root",
     "password": "",
     "database": "honeycomb",
     "host": "localhost",
-    "dialect": "mysql"
+    "dialect": 'mysql'|'sqlite'|'postgres'|'mssql'
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": 'mysql'|'sqlite'|'postgres'|'mssql'
   }
 };
