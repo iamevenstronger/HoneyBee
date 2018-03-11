7222d92deabaef713f4313bae878fd05d3b8cc4b
evenstronger <31339549+iamevenstronger@users.noreply.github.com>
2018-03-12 00:17:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -38,6 +38,15 @@ server.register([
         }
     });
 
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 server.route(routes);
 // Start the server
 server.start((err) => {
